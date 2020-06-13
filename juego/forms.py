beb42fd2e711e600ccae98b71e4f3e1e68fd2a31
Ramiro Nocelli <rjnocelli@Ramiros-MacBook-Pro.local>
2020-06-13 01:30:27 -0300

    CREAR FORM Crear Form (UserForm) Crear templates login.html, base.html, registracion.html
    Issue: #12

---
+++ juego/forms.py
@@ -0,0 +1,9 @@
+from juego.models import AvalonUser
+from django.contrib.auth.models import User
+
+class UserForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput())
+
+    class Meta():
+        model = User
+        fields = ('username')
